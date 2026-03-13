706e0089dc482ae0ab43dc036cfe83880f864b7a
Stanko K.R. <stanko@stanko.io>
2026-03-13 11:53:41 +0100

    Bump to 0.1.1

--- go/pkg/hey/version.go
+++ go/pkg/hey/version.go
@@ -1,7 +1,7 @@
 package hey
 
 // Version is the current version of the HEY Go SDK.
-const Version = "0.1.0"
+const Version = "0.1.1"
 
 // APIVersion is the HEY API version this SDK targets.
 const APIVersion = "2026-03-04"
